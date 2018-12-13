ca0277cdb3a9edc2f1dc05a77eb8bcaf848bfab9
brian delano <brian.delano@joyent.com>
2018-12-13 20:02:41 +0000

    changes to only fail on critical errors and cosmetics

--- updatedevice.pm
+++ updatedevice.pm
@@ -2,45 +2,72 @@ package PerlNetbox::updatedevice;
 use strict;
 use warnings;
 use JSON;
+use Time::HiRes qw(usleep ualarm gettimeofday tv_interval);
 use Data::Dumper;
 use FindBin;
 use File::Slurp;
+use File::Path qw(make_path remove_tree);
 use lib $FindBin::Bin;
 #local modules
 use CFG2JSON::Scrape;
 use PerlNetbox::netbox;
 use auth;
 sub new {
+  my $t0 = [gettimeofday];
   my $class=shift;
   my $args={ @_ };
   my $dcache={};
-  my $cachedir=$ENV{'HOME'}.'/pncache/';
-  my $logdir=$ENV{'HOME'}.'/pnlogs/';
+  my ($cachedir,$logdir);
+  if($args->{path}){
+    $cachedir=$args->{path}.'/cache/';
+    $logdir=$args->{path}.'/logs/';
+  }else{
+    $cachedir=$ENV{'HOME'}.'/netbox/cache/';
+    $logdir=$ENV{'HOME'}.'/netbox/logs/';
+  }
   my $cachefile=$cachedir.$args->{hostname}.'.json';
   my $logfile=$logdir.$args->{hostname}.'.log';
-  mkdir($cachedir) unless -d $cachedir; #create directory to hold our cache file
-  mkdir($logdir) unless -d $logdir; #create directory to hold the log output
+  make_path($cachedir) unless -d $cachedir; #create directory to hold our cache file
+  make_path($logdir) unless -d $logdir; #create directory to hold the log output
   open(STDOUT,">$logfile") or die $!;
   if(-f $cachefile){
     my $dcachejson=read_file($cachefile);
     $dcache=decode_json $dcachejson;
   }
   my $creds=auth->new($args->{client});
-  my $d=CFG2JSON::Scrape->new(filepath=>$args->{rancidpath},sitename=>$args->{sitename},hostname=>$args->{hostname});
-  my $device=$d->{device};
-  my $nb=PerlNetbox::netbox->new(token=>$creds->{token},host=>$creds->{host},device=>$device,dcache=>$dcache,debug=>1);
-  my $devret=$nb->updateDevice();
-  if ($nb->{error}){
-    print "ERRORS FOUND!\n";
-    print Dumper $nb->{error}
-  }else{
-    open(FILE,$cachefile) or die $!;
-    print FILE $d->json();
-    print "json file updated!\n";
+  my $d=CFG2JSON::Scrape->new(
+  filepath=>$args->{rancidpath},
+  sitename=>$args->{sitename},
+  hostname=>$args->{hostname}
+  );
+  my $nb=PerlNetbox::netbox->new(
+  token=>$creds->{token},
+  host=>$creds->{host},
+  device=>$d->{device},
+  dcache=>$dcache,
+  altinfo=>$args->{altinfo},
+  debug=>$args->{debug}
+  );
+  $nb->updateDevice();
+  if(!$nb->{error}{critical}){
+    $nb->updateInterfaces();
+    $nb->updateNats() if $d->{device}{nats};
+    $nb->updateConnections();
+    $nb->updateARP();
   }
+
+
+  if ($nb->{error}{critical}){
+    print "ERRORS FOUND!\n";
+    print Dumper $nb->{error};
+  }else{
+    print Dumper $nb->{error}{warning} if $nb->{error}{warning};
+    open(FILE,">$cachefile") or die $!;
+    print FILE $d->json();
+    #print "json file updated!\n";
+  }
+  print('> Full Device updated in '.sprintf("%.2fs\n", tv_interval ($t0)));
   return bless { (dev=>$d,nb=>$nb) },$class;
-
-
 }
 
 1;
